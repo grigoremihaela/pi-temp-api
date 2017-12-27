19149e13fca7ddc767fbf3ad17bbe0fe0bf0defb
Cezar <grigorecezar@yahoo.com>
2017-12-27 23:52:17 +0200

    migrate db

--- src/db/migrations/20171226223900_temp.js
+++ src/db/migrations/20171226223900_temp.js
@@ -2,8 +2,7 @@ exports.up = function(knex, Promise) {
 	return knex.schema.createTable('temp', table => {
 		table.increments('id').primary()
     table.string('temp')
-    table.timestamp('created_at')
-    table.timestamp('updated_at')
+    table.timestamp('created_at').defaultTo(knex.fn.now());
   });
 };
 
