3743d4348155deff16f22b045b497118ac57190f
Cezar <grigorecezar@yahoo.com>
2017-12-27 23:24:25 +0200

    add clear db

--- src/db/migrations/20171226223900_temp.js
+++ src/db/migrations/20171226223900_temp.js
@@ -2,8 +2,8 @@ exports.up = function(knex, Promise) {
 	return knex.schema.createTable('temp', table => {
 		table.increments('id').primary()
     table.string('temp')
-    table.timestamp('created_at').notNullable().defaultTo(knex.fn.now())
-    table.timestamp('updated_at').notNullable().defaultTo(knex.fn.now())
+    table.timestamp('created_at')
+    table.timestamp('updated_at')
   });
 };
 
