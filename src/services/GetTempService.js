00be747608547ef00b92ea4c478dee8c3207f6be
Cezar <grigorecezar@yahoo.com>
2018-01-07 16:10:27 +0200

    get temps from db where pins

--- src/services/GetTempService.js
+++ src/services/GetTempService.js
@@ -4,15 +4,32 @@ const knex = require('knex')(knexfile.production);
 class GetTempService {
   async get() {
     // returns all the ids of inserted rows
-    const temps1 = await knex('temp').where({
-    	                   sensorsUids: '28-0316a1a16fff'
+    const temps41 = await knex('temp').where({
+    	                   pin: 4,
+                         numberSensor: 1
                          }).orderBy('created_at', 'desc');
-    const temps2 = await knex('temp').where({
-    	                   sensorsUids: '28-03177067f9ff'
+    const temps42 = await knex('temp').where({
+                         pin: 4,
+                         numberSensor: 2 
                          }).orderBy('created_at', 'desc');
+    const temps43 = await knex('temp').where({
+                         pin: 4,
+                         numberSensor: 3
+                         }).orderBy('created_at', 'desc');
+    const temps44 = await knex('temp').where({
+                         pin: 4,
+                         numberSensor: 4
+                         }).orderBy('created_at', 'desc');
+    const temps17 = await knex('temp').where({
+    	                   pin: 17
+                         }).orderBy('created_at', 'desc');
+    const temps22 = await knex('temp').where({
+                         pin: 22
+                         }).orderBy('created_at', 'desc');
+    const temps = [temps41[0], temps42[0], temps43[0], temps44[0], temps17[0], temps22[0]];
 
     // we inserted just one so return first one
-    return [temps1[0], temps2[0]];
+    return temps;
   }
 }
 
