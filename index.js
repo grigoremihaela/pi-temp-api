b4f0f6652d9df8653f695bfeedcc95860b0e316e
Mihaela Grigore <Mihaela@Cezars-MacBook-Air.local>
2018-02-08 21:15:57 +0200

    add headers

--- index.js
+++ index.js
@@ -28,6 +28,13 @@ require('express-async-errors');
 
 app.use(bodyParser.json());
 
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 // Add headers
 app.use(function (req, res, next) {
 
